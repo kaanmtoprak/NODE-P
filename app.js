1594c7a383c5623b37ef0d3874f103bb7cf32d77
Mehmet Kaan Toprak <kaanmehtoprak@gmail.com>
2022-11-07 12:29:10 +0300

    some changes

--- app.js
+++ app.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const ejs = require('ejs');
 require('dotenv').config();
 const path = require('path');
 
 const app = express();
-app.use(express.static('public'));
 
+// temlpate engine
+
+app.set("view engine", "ejs");
+
+//middlewares
+app.use(express.static('public'));
+ 
+
+// Routes
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("index");
 });
+app.get('/about', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("about");
+});
+app.get('/add', (req, res) => {
+  // res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render("add");
+});
+
 
 app.listen(process.env.PORT, () => {
   console.log(`Sunucu ${process.env.PORT} portunda başlatıldı...`);
